bd7002498e34dc36c17ba601a28ac10f63cf077b
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-11-27 18:23:35 -0800

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,7 @@ class Cart < ActiveRecord::Base
   def total
     total = 0
     self.items.each do |item|
-      total += item.price
+      total += (item.price * item.line_items.find_by(:item_id => item.id).quantity)
     end
     total
   end
@@ -15,7 +15,6 @@ class Cart < ActiveRecord::Base
     line_item = self.line_items.find_by(:item_id => item_id)
       if line_item
         line_item.quantity += 1
-        line_item
       else
         line_item=self.line_items.build(:item_id => item_id, :quantity => 1)
       end
